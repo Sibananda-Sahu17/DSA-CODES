4253a46fe1f5dc5afcc6027493a7458573315f65
Sibananda Sahu <sibananda6481@gmail.com>
2023-03-30 18:14:19 +0530

    q4

--- RECURSION/q4_subsets.java
+++ RECURSION/q4_subsets.java
@@ -7,6 +7,7 @@ public class subsets {
         for(int i=0; i<n; i++){
             arr[i] = sc.nextInt();
         }
+        
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> subset = new ArrayList<>();
         helper(arr, 0, ans, subset);
