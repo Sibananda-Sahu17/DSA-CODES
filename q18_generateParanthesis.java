1339cc1dde4c702eb89d5eb66664f2931a625baa
Sibananda Sahu <sibananda6481@gmail.com>
2023-04-01 23:13:25 +0530

    q18

---
+++ q18_generateParanthesis.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class q18_generateParanthesis {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        ArrayList<String> ans = new ArrayList<>();
+        paranthesis(n, ans, "", 0, 0);
+
+        System.out.println(ans);
+    }
+    public static void paranthesis(int n, ArrayList<String> ans, String s, int open , int close){
+        if(s.length()==n*2){
+            ans.add(s);
+            return;
+        }
+
+        if(open<n){
+            paranthesis(n, ans, s+'(', open+1, close);
+        }
+        if(close<open){
+            paranthesis(n, ans, s+')', open, close+1);
+        }
+    }
+}
